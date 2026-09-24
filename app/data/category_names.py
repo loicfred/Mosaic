5219fd3e87ad67f90676204a250af1f4902c9c7b
Loïc Fred <loic.cheerkoot@gmail.com>
2026-09-24 10:44:26 +0400

    Update

--- app/data/category_names.py
+++
@@ -1,9 +0,0 @@
-"""Translate Olist product categories while preserving unknown values for model features."""
-
-import pandas as pd
-
-
-def with_category_name(products: pd.DataFrame, translation: pd.DataFrame) -> pd.DataFrame:
-    products = products.merge(translation, on="product_category_name", how="left")
-    products["category"] = products["product_category_name_english"].fillna(products["product_category_name"])
-    return products
